8d3ff4071d3dbb1f6dbb6d0614b8f1b12b5a80ff
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-04-30 17:59:29 -0400

    minor refactoring and readability changes

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
 const ones = [
-  "",
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine"
+  ``,
+  `one`,
+  `two`,
+  `three`,
+  `four`,
+  `five`,
+  `six`,
+  `seven`,
+  `eight`,
+  `nine`
 ];
 const tens = [
-  "",
-  "ten",
-  "twenty",
-  "thirty",
-  "fourty",
-  "fifty",
-  "sixty",
-  "seventy",
-  "eighty",
-  "ninety"
+  ``,
+  `ten`,
+  `twenty`,
+  `thirty`,
+  `fourty`,
+  `fifty`,
+  `sixty`,
+  `seventy`,
+  `eighty`,
+  `ninety`
 ];
 const teens = [
-  "ten",
-  "eleven",
-  "twelve",
-  "thirteen",
-  "fourteen",
-  "fifteen",
-  "sixteen",
-  "seventeen",
-  "eighteen",
-  "nineteen"
+  `ten`,
+  `eleven`,
+  `twelve`,
+  `thirteen`,
+  `fourteen`,
+  `fifteen`,
+  `sixteen`,
+  `seventeen`,
+  `eighteen`,
+  `nineteen`
 ];
 
 function convertHunchos(n) {
   return n === 1000 ? `one thousand`
     : n === 0 ? `zero`
-    : n > 99 ? `${ones[Math.floor(n / 100)]} hundred ${convertTens(n % 100)}`
+    : n > 99 ? ` ${ones[Math.floor(n / 100)]} hundred ${convertTens(n % 100)} `
     : convertTens(n);
 }
 
 function convertTens(n) {
   return n < 10 ? ones[n] 
     : n >= 10 && n < 20 ? teens[n - 10]
-    : `${tens[Math.floor(n / 10)]} ${ones[n % 10]}`;
+    : ` ${tens[Math.floor(n / 10)]} ${ones[n % 10]} `;
 }
 
 function makeText(content) {
-  const div = document.createElement(`div`);
+  const div  = document.createElement(`div`);
   div.textContent = content;
   document.body.appendChild(div);
 }
