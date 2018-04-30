e1f236b4a12051746beb3361d3b4ef995fa2847e
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-04-30 17:52:37 -0400

    minor refactoring and readability changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const single = [
+const ones = [
   "",
   "one",
   "two",
@@ -35,36 +35,27 @@ const teens = [
   "nineteen"
 ];
 
-function makeText(content) {
-  const div = document.createElement("div");
-  div.textContent = content;
-  //  const wrap = document.getElementById('wrap');
-  document.body.appendChild(div);
-}
-
 function convertHunchos(n) {
-  return n === 1000
-    ? "one thousand"
-    : n > 99
-      ? `${single[Math.floor(n / 100)]} hundred ${convertTens(n % 100)}`
-      : convertTens(n);
+  return n === 1000 ? `one thousand`
+    : n === 0 ? `zero`
+    : n > 99 ? `${ones[Math.floor(n / 100)]} hundred ${convertTens(n % 100)}`
+    : convertTens(n);
 }
 
 function convertTens(n) {
-  return n < 10
-    ? single[n]
-    : n >= 10 && n < 20
-      ? teens[n - 10]
-      : `${tens[Math.floor(n / 10)]} ${single[n % 10]}`;
+  return n < 10 ? ones[n] 
+    : n >= 10 && n < 20 ? teens[n - 10]
+    : `${tens[Math.floor(n / 10)]} ${ones[n % 10]}`;
 }
 
-function convert(n) {
-  return n === 0 ? "zero" : convertHunchos(n);
+function makeText(content) {
+  const div = document.createElement(`div`);
+  div.textContent = content;
+  document.body.appendChild(div);
 }
 
 function runNumbers(n) {
   for (let i = 1; i <= n; i++) {
-    const n = convert(i);
-    makeText(n);
+    makeText(convertHunchos(i));
   }
 }
